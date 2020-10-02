a792a6ea76c803c965370764e432d892e859b42a
akartis-dev <akartis-dev@sitraka.com>
2020-10-02 20:41:47 +0300

    [USER feat]: build a basic member page

--- src/Security/TodoAuthenticator.php
+++ src/Security/TodoAuthenticator.php
@@ -96,8 +96,9 @@ class TodoAuthenticator extends AbstractFormLoginAuthenticator implements Passwo
             return new RedirectResponse($targetPath);
         }
 
+        return new RedirectResponse($this->urlGenerator->generate('member.index'));
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+//        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
